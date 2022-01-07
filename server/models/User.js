2b35d431411704670430a504585ce7cfc041ff97
MOMBODARWIN <76207354+MOMBODARWIN@users.noreply.github.com>
2022-01-08 00:00:39 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,21 @@
 const { Model, DataTypes } = require("sequelize");
 const connection = require("../lib/db");
+const bcrypt = require("bcryptjs");
 
-class User extends Model {}
+class User extends Model { }
 
 User.init(
   {
-    lastname: DataTypes.STRING,
-    firstname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    lastname: { type: DataTypes.STRING, allowNull: false },
+    firstname: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, validate: { isEmail: true } },
+    password: { type: DataTypes.STRING, allowNull: false },
+    createdAt: { Type: DataTypes.DATE, allowNull: false },
+    updateAt: { Type: DataTypes.DATE, allowNull: false },
+    deleteAt: { Type: DataTypes.DATE, allowNull: false, default: false },
   },
   {
     sequelize: connection,
     modelName: "User",
   }
 );
-
-connection.sync().then(() => {
-  console.log("Database synced");
-});
-
-module.exports = User;
