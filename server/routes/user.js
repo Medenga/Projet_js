c24c11be1b4980dfbea7fd9face25a18046f9e73
Manu-92 <76664813+Manu-92@users.noreply.github.com>
2022-01-07 14:57:26 +0100

    Add files via upload
    Inscription : Un utilisateur s'inscrit avec un lastname, un firstname, un email, un password

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,47 +1,65 @@
-const express = require("express");
+const { Router } = require("express");
 const User = require("../models/User");
-const router = express.Router();
+const router = new Router();
 
-router.get("", (req, res) => {
+router.get("/users", (req, res) => {
   const criteria = req.query;
   User.findAll({
     where: criteria,
-  }).then((users) => res.json(users));
+  }).then((users) => {
+    res.json(users);
+  });
 });
 
-router.post("", (req, res) => {
-  User.create(req.body).then((user) => res.status(201).json(user));
+router.post("/lastname", (req, res) => {
+  User.create(req.body).then((user) => {
+    res.status(201).json(user);
+  });
 });
 
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+router.post("/firstname", (req, res) => {
+  User.create(req.body).then((user) => {
+    res.status(201).json(user);
+  });
+});
+
+router.post("/email", (req, res) => {
+  User.create(req.body).then((user) => {
+    res.status(201).json(user);
+  });
+});
+
+router.post("/password", (req, res) => {
+  User.create(req.body).then((user) => {
+    res.status(201).json(user);
+  });
+});
+
+router.get("/users/:id", (req, res) => {
+  const id = req.params.id;
   User.findByPk(id).then((user) => {
     if (!user) res.sendStatus(404);
     else res.json(user);
   });
 });
 
-router.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  User.destroy({
-    where: {
-      id: id,
-    },
-  }).then((nbDeleted) => {
-    if (!nbDeleted) res.sendStatus(404);
-    else res.sendStatus(204);
-  });
-});
-
-router.put("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+router.put("/users/:id", (req, res) => {
+  const id = req.params.id;
   User.update(req.body, {
-    where: {
-      id: id,
-    },
+    where: { id: id },
   }).then(([nbUpdated]) => {
     if (!nbUpdated) res.sendStatus(404);
     else User.findByPk(id).then((user) => res.json(user));
+  });
+});
+
+router.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  User.destroy({
+    where: { id: id },
+  }).then((nbDeleted) => {
+    if (!nbDeleted) res.sendStatus(404);
+    else res.sendStatus(204);
   });
 });
 
