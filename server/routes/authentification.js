992b50d428a8220795d0cf5a2f2ab9c6ff94cc54
FlorianCesaireHierso <95698646+FlorianCesaireHierso@users.noreply.github.com>
2022-01-07 23:36:12 +0100

    Update authentification.js

--- server/routes/authentification.js
+++ server/routes/authentification.js
@@ -1,18 +1,39 @@
-const { Router } = require("express");
+const router = require("express").Router();
 const User = require("../models/User");
-const router = require("./user");
+const bcrypt = require("bcrypt");
 
-//creer un compte
-router.post("/créer un compte", async(req,res)=>{
-    try{
-        const inscription= new User({
-            lastname: req.body.lastname,
-            firstname: req.body.firstname,
-            email: req.body.email,
-            password: req.body.password,
-        }
-    }catch(err){
-        res.status(500).json(err);
-    }
-})
-//connexion
+//Creer un compte
+router.post("/inscripyion", async (req, res) => {
+  try {
+    const token = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+    const inscription = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hash,
+    });
+
+    const user = await insciption.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Se connecter
+router.post("connexion", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json();
+
+    const code = await bcrypt.compare(req.body.password, user.password);
+    !code && res.status(400).json();
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
